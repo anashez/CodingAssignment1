414b0c1bbb88b86f24108b8fb6654e9335a89b77
Anashez Shako <anashez.s@gmail.com>
2024-01-24 13:34:12 +0200

    implement entire accounts

--- Java/src/TheBank.java
+++ Java/src/TheBank.java
@@ -1,3 +1,26 @@
 public class TheBank implements IBank{
+    @Override
+    public void OpenAccount(IAccount account) {
 
+    }
+
+    @Override
+    public void CloseAccount(int accountNumber) {
+
+    }
+
+    @Override
+    public List<IAccount> GetAllAccounts() {
+        return null;
+    }
+
+    @Override
+    public List<IAccount> GetAllAccountsInDebt() {
+        return null;
+    }
+
+    @Override
+    public List<IAccount> GetAllAccountsWithBalance(double balanceAbove) {
+        return null;
+    }
 }
